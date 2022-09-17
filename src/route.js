1efda505df2d3cbaf827bb939a9ba42c56d61604
Ahlam77 <78358712+Ahlam77@users.noreply.github.com>
2022-09-17 21:52:12 +0200

    Update route.js

--- src/route.js
+++ src/route.js
@@ -1,14 +1,23 @@
 const express = require('express')
+const fs = require('fs')
 const ControllerXml2Xlsx = require('./controllers/ControllerXml2Xlsx.js')
-
-//const data = require('./data/data.json')
-
+const xlsxPath = "./public/data/data.xlsx";
 const route = express.Router()
 
 route.get('/', (req, res) => res.render('index'))
 
-route.get('/json', (req, res) => res.render('json'))
-
+route.get("/json", (req, res) => res.render("index"));
+route.get("/download", (req, res) => {
+  res.download(xlsxPath,(err)=>{
+    if (err){
+        fs.unlinkSync(xlsxPath);
+        res.send("Unable to download");
+    }
+    fs.unlinkSync(xlsxPath);
+  });
+  
+});
+ 
 route.post('/convert', ControllerXml2Xlsx.convertFromXmlToXlsx)
 
 
